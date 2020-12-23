ced4d6c9db469eddf684c2a039215d1bbfa6b8f3
Neil Carlo Sucuangco <necafasu@gmail.com>
2020-12-23 16:45:05 +0000

    add temperature conversion

---
+++ tests/TemperatureTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Faisuc\UnitConversions\Tests;
+
+use Faisuc\UnitConversions\Temperature;
+use PHPUnit\Framework\TestCase;
+
+class TemperatureTest extends TestCase
+{
+    /** @test */
+    public function it_can_convert_celsius_to_fahrenheit()
+    {
+        $fahrenheit = Temperature::fromCelsius(100)->toFahrenheit();
+
+        $this->assertEquals(212, $fahrenheit);
+    }
+}
